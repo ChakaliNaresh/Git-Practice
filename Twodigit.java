526ce07c357441027416c8e65a2c892779f27644
ChakaliNaresh <nareshchakali662001@gmail.com>
2023-06-01 11:41:02 +0530

    Twodigit.java

---
+++ Twodigit.java
@@ -0,0 +1,17 @@
+
+public class Hello {
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	    System.out.println("Enter the two digit number");
+	    int n=sc.nextInt();
+	    int d1=n%10;
+	    int d2=n/10;
+	    int result=d1+d2+d1*d2;
+	    if(result==n)
+	    {
+	    	System.out.println("it is the Special two digit number");
+	    }
+	    else 
+	    {
+	    	System.out.println("it is not a special two digit number");	 
+   }
